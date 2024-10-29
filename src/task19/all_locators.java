94d0fe1457de3dc94e35b8891a83a1781b110909
Sundar7323 <textmesundar@gmail.com>
2024-10-29 07:31:56 +0530

    task19

--- src/task19/all_locators.java
+++ src/task19/all_locators.java
@@ -14,17 +14,16 @@ public class all_locators {
 	//	chrome.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		chrome.get("https://www.guvi.in/register");
 		
-		//ID:
+		//clicking the login link on the right side
+		chrome.findElement(By.xpath("//a[@class='login']")).click();
+		chrome.navigate().back();
+		
 		chrome.findElement(By.id("name")).sendKeys("Dinesh");
-		//CSS Selector
 		chrome.findElement(By.cssSelector("input#email")).sendKeys("testguvi@hotmail.com");
-		//XPATH
 		chrome.findElement(By.xpath("//input[@type=\"password\"]")).sendKeys("dineshtest09_");
+		chrome.findElement(By.xpath("//input[@class='form-control countrycode-left']")).sendKeys("9098929892");
+		chrome.findElement(By.id("signup-btn")).click();
 		
-		
-		chrome.findElement(By.tagName("input")).sendKeys("9098929892");
-		//CLASS NAME
-		//chrome.findElement(By.className("btn signup-btn")).click();
 	
 	}
 
